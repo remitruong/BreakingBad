e70a7d46a49a7ffcfdc1a1c5caf76adf2f044c4b
Remi <remi.truong98@gmail.com>
2019-11-21 21:49:06 +0100

    Fragment

--- app/src/main/java/com/example/breakingbad/view/MainActivity.java
+++ app/src/main/java/com/example/breakingbad/view/MainActivity.java
@@ -1,20 +1,18 @@
 package com.example.breakingbad.view;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import com.example.breakingbad.Injection;
 import com.example.breakingbad.R;
-import com.example.breakingbad.controller.MainController;
-import com.example.breakingbad.model.BreakingBadCharacter;
+
 
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     //Déclaration des variables.
     private RecyclerView recyclerView;
@@ -25,25 +23,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = this.getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
-        MainController controller = new MainController(this, Injection.getInstance(), sharedPreferences);
-        controller.start();
+        loadFragment(new CharacterFragment());
     }
 
-    public void showList(List<BreakingBadCharacter> breakingBadCharacterList) {
-        //Initialisation de la variable recyclerView
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        //Optimisation des performances Merci la documentation.
-        recyclerView.setHasFixedSize(true);
-        // Layout Manager = Manage l'affichage. Ici en liste Verticale
-        //Initialisation de la variable layoutManager
-        layoutManager = new LinearLayoutManager(this);
-
-        recyclerView.setLayoutManager(layoutManager);
-        // define an adapter
-
-        //Initialisation de la variable mAdapter
-        mAdapter = new MyAdapter(breakingBadCharacterList);
-        recyclerView.setAdapter(mAdapter);
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+
 }
